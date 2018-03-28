7daa451a6de516d16bcb30cafd5003e72e3ef6ad
Becky Arnold <r.j.arnold.uk@gmail.com>
2018-03-28 14:18:20 +0100

    Refactored infor_from_user.py

--- info_from_user.py
+++ info_from_user.py
@@ -1,18 +1,46 @@
-import rasterio
-from types import FunctionType
-#import matplotlib.pyplot as plt
+# This function checks if a valid filename has been provided
+def check_invalid_filename(filename):
+
+    # List of valid filetypes
+    valid_filetypes = ['gen', 'thf', 'adf', 'blx', 'xlb', 'bag', 'bmp', 'kap',
+                   'bt', 'doq', 'dt0', 'dt1', 'dt2', 'ers', 'n1', 'fits', 
+                   'hdr', 'gif', 'grb', 'tif', 'img', 'mpr', 'mpl', 'mem', 
+                   'jpg', 'jp2', 'j2k','ntf', 'nsf', 'grc', 'tab', 'grd',
+                   'png', 'ppm', 'pgm', 'prf', 'rik', 'rsw', 'mtw', 'xml',
+                   'safe', 'ter', 'TIL', 'vrt', 'xpm']
+
+    invalid = False
+
+    # Check the input is a file 
+    if '.' not in filename:
+        print('\n\nThat is not a file.')
+        invalid = True
+
+    # If it is a file check it's a valid file type
+    else:
+        filetype = filename[filename.rfind('.')+1:]
+        if filetype not in valid_filetypes:
+            invalid = True
+            print('\n\nInvalid filetype. Your filetype is ', filetype, '.\n\nValid filetypes are:', '\n\n', valid_filetypes)
+    
+    return invalid
 
 
+# ________________________________________________
 
-# This function checks if the doi th user has supplied is invalid
+
+# This function checks if the doi the user has supplied is invalid
 def check_invalid_doi(doi):
 
     if ('10.' not in doi) or ('/' not in doi):
+        print('\n\nThat is an invalid doi, please supply a valid doi.')
         invalid = True
     else:
         invalid = False
 
     return invalid
+
+# ________________________________________________
 
 '''
 This funtction asks the user for:
@@ -23,7 +51,7 @@ This funtction asks the user for:
 It also sanity-check the supplied information e.g. valid filetype,
 And keeps asking until a reasonable answer has been provided.
 '''
-def info_from_user(valid_filetypes):
+def info_from_user():
 
     # Ask the user for their filename
     # Keep asking until a valid file has been given
@@ -32,56 +60,40 @@ def info_from_user(valid_filetypes):
     while ask:
 
         # Get name from user
-        filename = input('Input the filename. ')#'sample_data/subset_data_1/subset_data_1.tif'
+        filename = input('\n\nInput the filename. ')
 
-        # Check a valid response has been given
-        valid = True
+        ask = check_invalid_filename(filename)
 
-        # Check the input is a file 
-        if '.' not in filename:
-            print('That is not a file.')
-            valid = False
-
-        # If it is a file check it's a valid file type
-        else:
-            filetype = filename[filename.rfind('.')+1:]
-            if filetype not in valid_filetypes:
-                valid = False
-                print('Invalid filetype. Your filetype is ', filetype, '.\n\nValid filetypes are:', '\n\n\n', valid_filetypes)
-
-        ask = not valid
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-    # Ask the user for the doi of their dataset and publication
-    # Keep asking until a valid dois has been given
+    # Ask the user for the doi of their dataset
+    # Keep asking until a valid doi has been given
     
     ask = True
     while ask:
 
         # Get doi from user
-        doi_dataset = input('Input the dataset doi. ')
+        doi_dataset = input('\n\nInput the dataset doi. ')
         ask = check_invalid_doi(doi_dataset)
 
+
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    # Ask the user for the doi of their publication
+    # Keep asking until a valid doi has been given
     ask = True
     while ask:
 
         # Get doi from user
-        doi_pub = input('Input the publication doi. ')
+        doi_pub = input('\n\nInput the publication doi. ')
         ask = check_invalid_doi(doi_pub)
 
     return filename, doi_dataset, doi_pub
 
-# List of valid filetypes
-valid_filetypes = ['gen', 'thf', 'adf', 'blx', 'xlb', 'bag', 'bmp', 'kap',
-                   'bt', 'doq', 'dt0', 'dt1', 'dt2', 'ers', 'n1', 'fits', 
-                   'hdr', 'gif', 'grb', 'tif', 'img', 'mpr', 'mpl', 'mem', 
-                   'jpg', 'jp2', 'j2k','ntf', 'nsf', 'grc', 'tab', 'grd',
-                   'png', 'ppm', 'pgm', 'prf', 'rik', 'rsw', 'mtw', 'xml',
-                   'safe', 'ter', 'TIL', 'vrt', 'xpm']
 
 # Get required information from the user.
-filename, doi_dataset, doi_pub = info_from_user(valid_filetypes)
+filename, doi_dataset, doi_pub = info_from_user()
 
