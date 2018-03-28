fa6f54bf2e0c6d20960c6e5e736c412e2076d2ee
Brigitta Sipocz <bsipocz@gmail.com>
2018-03-28 12:31:52 +0100

    Adding some docs

--- geometa/geometa.py
+++ geometa/geometa.py
@@ -5,8 +5,25 @@ import rasterio.warp
 import rasterio.features
 
 
-def get_meta(datafile, source_info=None, outputfile=None):
+def get_meta(datafile, dataset_doi=None, publication_doi=None,
+             outputfile=None):
+    """
+    Generating geospatial metadata from Digital Elevation Model files.
 
+    Parameters
+    ----------
+    datafile : str
+        Filename for data file. Format should be supported by the
+        rasterio packgage.
+    dataset_doi : str
+        DOI for the original raw data
+    publication_doi : str
+        Reference publication for this dataset
+    outputfile : str
+        Name of output file to dump the metadata information
+
+
+    """
     with rasterio.open(datafile) as data:
         # Read the dataset's valid data mask as a ndarray.
         mask = data.dataset_mask()
@@ -18,12 +35,12 @@ def get_meta(datafile, source_info=None, outputfile=None):
             # Transform shapes from the dataset's own coordinate
             # reference system to CRS84 (EPSG:4326).
             geo_info = rasterio.warp.transform_geom(
-                data.crs, 'EPSG:26917', geo_info, precision=6)
+                data.crs, data.crs, geo_info, precision=6)
 
     metadata = {
         'georeferencing': {
             'spatial extent': geo_info,
-            'EPSG': 26917},
+            'EPSG': data.crs.to_string()},
         'driver': data.driver,
         'width': data.width,
         'height': data.height,
@@ -32,11 +49,15 @@ def get_meta(datafile, source_info=None, outputfile=None):
         'crs': data.crs.to_string(),
         'transform': data.transform,
         'nodata': data.nodata,
-        'bounding_box': data.bounds
+        'bounding_box': data.bounds,
+        'source info': {}
     }
 
-    if source_info is not None:
-        metadata['source info'] = source_info
+    if dataset_doi is not None:
+        metadata['source info']['dataset DOI'] = source_info
+
+    if publication_doi is not None:
+        metadata['source info']['publication DOI'] = publication_doi
 
     if outputfile:
         with open(outputfile, 'w') as of:
