b19ad04eba3e7b23f821d29e2f0b536a6113d92e
Anastasis Georgoulas <a.georgoulas@ucl.ac.uk>
2018-03-28 14:38:25 +0100

    Clean up style and typos.

--- info_from_user.py
+++ info_from_user.py
@@ -2,16 +2,17 @@
 def check_invalid_filename(filename):
 
     # List of valid filetypes
-    valid_filetypes = ['gen', 'thf', 'adf', 'blx', 'xlb', 'bag', 'bmp', 'kap',
-                   'bt', 'doq', 'dt0', 'dt1', 'dt2', 'ers', 'n1', 'fits', 
-                   'hdr', 'gif', 'grb', 'tif', 'img', 'mpr', 'mpl', 'mem', 
-                   'jpg', 'jp2', 'j2k','ntf', 'nsf', 'grc', 'tab', 'grd',
+    valid_filetypes = [
+                   'gen', 'thf', 'adf', 'blx', 'xlb', 'bag', 'bmp', 'kap',
+                   'bt', 'doq', 'dt0', 'dt1', 'dt2', 'ers', 'n1', 'fits',
+                   'hdr', 'gif', 'grb', 'tif', 'img', 'mpr', 'mpl', 'mem',
+                   'jpg', 'jp2', 'j2k', 'ntf', 'nsf', 'grc', 'tab', 'grd',
                    'png', 'ppm', 'pgm', 'prf', 'rik', 'rsw', 'mtw', 'xml',
                    'safe', 'ter', 'TIL', 'vrt', 'xpm']
 
     invalid = False
 
-    # Check the input is a file 
+    # Check the input is a file
     if '.' not in filename:
         print('\n\nThat is not a file.')
         invalid = True
@@ -21,8 +22,9 @@ def check_invalid_filename(filename):
         filetype = filename[filename.rfind('.')+1:]
         if filetype not in valid_filetypes:
             invalid = True
-            print('\n\nInvalid filetype. Your filetype is ', filetype, '.\n\nValid filetypes are:', '\n\n', valid_filetypes)
-    
+            print('\n\nInvalid filetype. Your filetype is ', filetype,
+                  '.\n\nValid filetypes are:', '\n\n', valid_filetypes)
+
     return invalid
 
 
@@ -42,45 +44,40 @@ def check_invalid_doi(doi):
 
 # ________________________________________________
 
-'''
-This funtction asks the user for:
--The name of the file to be converted
--The doi of the dataset
--The doi of the publication
 
-It also sanity-check the supplied information e.g. valid filetype,
-And keeps asking until a reasonable answer has been provided.
-'''
 def info_from_user():
+    '''
+    This function asks the user for:
+    -The name of the file to be converted
+    -The doi of the dataset
+    -The doi of the publication
 
+    It also sanity-checks the supplied information e.g. valid filetype,
+    and keeps asking until a reasonable answer has been provided.
+    '''
     # Ask the user for their filename
     # Keep asking until a valid file has been given
-    
+
     ask = True
     while ask:
 
         # Get name from user
-        filename = input('\n\nInput the filename. ')
+        filename = input('Input the filename: ')
 
         ask = check_invalid_filename(filename)
 
-    
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 
     # Ask the user for the doi of their dataset
     # Keep asking until a valid doi has been given
-    
     ask = True
     while ask:
 
         # Get doi from user
-        doi_dataset = input('\n\nInput the dataset doi. ')
+        doi_dataset = input('Input the dataset doi: ')
         ask = check_invalid_doi(doi_dataset)
 
-
     # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 
     # Ask the user for the doi of their publication
     # Keep asking until a valid doi has been given
@@ -88,12 +85,12 @@ def info_from_user():
     while ask:
 
         # Get doi from user
-        doi_pub = input('\n\nInput the publication doi. ')
+        doi_pub = input('Input the publication doi: ')
         ask = check_invalid_doi(doi_pub)
 
     return filename, doi_dataset, doi_pub
 
 
-# Get required information from the user.
-filename, doi_dataset, doi_pub = info_from_user()
-
+if __name__ == "__main__":
+    # Get required information from the user.
+    filename, doi_dataset, doi_pub = info_from_user()
